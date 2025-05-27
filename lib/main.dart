3f4b14ee46c4d6742dc2fc7f1ba94356aa7d0e4c
Muzaking <muzammilflash213@gmail.com>
2025-05-27 23:18:57 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
 import 'view/login_view.dart';
 
-void main() => runApp(
-  MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: LoginView(),
-  )
-);
+void main() => runApp(MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: LoginView(),
+    ));
+//hooo
