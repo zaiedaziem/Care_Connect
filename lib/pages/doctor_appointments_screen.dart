8a6cc4d6a64f4ed1407f82cb2c8364a7e55ee1cd
zaiedaziem <143258091+zaiedaziem@users.noreply.github.com>
2025-07-03 08:52:22 +0800

    Update doctor_appointments_screen.dart

--- lib/pages/doctor_appointments_screen.dart
+++ lib/pages/doctor_appointments_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import '../services/AppointmentService.dart';
+import '../services/appointment_service.dart';
 import '../models/appointment_model.dart';
 
 class DoctorAppointmentsScreen extends StatefulWidget {
